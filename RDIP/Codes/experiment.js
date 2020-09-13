a8b37187ca7b114873d8d96e31d2de8933b9a170
KrishnaCh97 <chskbharath@gmail.com>
2020-09-13 12:05:59 +0530

    Adding Extra Functionalities

--- RDIP/Codes/experiment.js
+++ RDIP/Codes/experiment.js
@@ -22,6 +22,7 @@ var randWord;
 var sentence = [];
 var sent;
 var lan;
+var array;
 
 function randomSentence(){
 	lan = document.getElementById('language').value;
@@ -44,12 +45,59 @@ function randomWord(sent){
 	}
 	return temp;
 }
+function intro(){
+	reset('heading',"Introduction");
+	document.getElementById('language').style.display = "none";
+	resetAll();
+	reset('obj',"A sentence can become more complex, if more than one verb is present or by joining two sentences or words using conjunctions or by some other methods.<br><br>In this experiment also, you will make more difficult sentences using the given words.");
+}
+function theory(){
+	reset('obj',"");
+	reset('heading',"Theory");
+	document.getElementById('language').style.display = "none";
+	resetAll();
+	reset('quiz1',"<u>Clause</u><br>A clause typically contains a subject noun phrase and a finite verb. Some languages allow subjects to be omitted. There are two types of subclauses:<ul><li>independent clause</li><li>subordinate clause</li></ul>Independent clause shows the complete meaning in it. For example: Ram eats. A subordinate clause is not a complete sentence. For example: because I am sick. Sentences can also be classified on the basis of clauses.");
+	reset('quiz2',"Classification on the basis of clauses are:<ol><li>A <b>simple sentence</b> consists of only one independent clause. There are no subordinate clauses.</li><li>A <b>compound sentence</b> consists of more than one independent clauses joined ny conjunctions or punctuations. There are no subordinate clauses.</li><li>A <b>complex sentence</b> consists of atleast one indpendent clause and a single subordinate clause</li><li>A <b>complex-compound sentence</b> consists of more than one independent clauses and atleast one of the independent clauses has one or more than one subordinate clauses</li></ol>")
 
+}
+function objective(){
+		reset('obj',"");
+	resetAll();
+	document.getElementById('language').style.display = "none";
+	reset('heading',"Objective");
+	reset('obj',"<hr><br><br>The objective of this experiment is to know how to form logically correct sentences from the given words.<br><br><br><hr>")
+
+}
+function experiment(){
+	resetAll();
+	reset('heading',"Experiment");
+	reset('obj',"");
+	document.getElementById('language').style.display = "block";
+	document.getElementById('language').style.margin = "0 40%";	
+}
+
+function quiz(){
+	resetAll();
+		reset('obj',"");
+	reset('heading',"Quizzes");
+	document.getElementById('language').style.display = "none";
+	reset('quiz1',"Which of these is a valid sentence?<ol><li>Ram came after lunch.</li><li>Ram came after having lunch.</li><li>Sleeping I saw a tiger.</li><li>I saw a sleeping tiger.</li><li>No Parking is allowed.</li></ol>");
+	reset('quiz2',"<ol><li>खाते खाते राम सो गया</li><li>राम खाते खाते सो गया</li><li>राम खाता खाते सो गया</li><li>राम खाकर सो गया</li><li>राम और श्याम ने भी खाना खाया</li><li>राम ने और श्याम भी खाना खाया</li>")	
+}
+
+function procedure(){
+	resetAll();
+	reset('obj',"");
+	reset('heading',"Procedure");
+	document.getElementById('language').style.display = "none";
+	reset('quiz1',"<u><b>STEP 1:</b></u> Select a language which you know better<br><u><b>STEP 2:</b></u> Select the buttons which has words written on it, in a proper order<br><u><b>OUTPUT:</b></u> Group of words in a selected order will be shown<br>");
+	reset('quiz2',"<u><b>NOTE:</b></u><ol><li>If a wrong sentence is formed,<button>Re-form the sentence</button>is available for re-setting.<li>You can check whether the formed sentence is a valid or not by clicking<button>Check the correctness of this sentence</button></li><li>For a wrong sentence, you can get the correct sentence by clicking<button>Get correct sentence</button></li>")
+}
 function show(){
 var text = "Form a sentence (Declarative or Interrogative or any other type) from the given words<br>(select the buttons in proper order)";
 var lan = document.getElementById('language').value;
 if((lan==="English")||(lan==="Hindi")){
-	document.getElementById('obj').innerHTML = text;
+	reset('obj',text);
 	var sent = randomSentence();
 	randWord = randomWord(sent);
 	var set = formSent(randWord);
@@ -59,26 +107,29 @@ else{
 }
 }
 function formSent(){
-	document.getElementById('randText').innerHTML = "";
-	document.getElementById('answerArea').innerHTML = "";
-	document.getElementById('ansHead').innerHTML = "";
+	resetAll();
 	for(i=0;i<randWord.length;i++){
 	document.getElementById('randText').innerHTML += ("<button id='btn"+i+"' value='"+randWord[i]+"' onclick='addWord("+i+")'>"+randWord[i]+"</button>")
 	}
 }
-function reformSent(){
-	formSent();
-	sentence = [];
-	document.getElementById('getCorrect').innerHTML = "Get Correct Sentence";
+function resetAll(){
+	reset('getCorrect',"Get Correct Sentence");
+	reset('rightResult',"");
+	reset('wrongResult',"");
+	reset('correctSentence',"");
+	reset('ansHead',"");
+	reset('randText',"");
+	reset("answerArea","");
+	reset('quiz1',"");
+	reset('quiz2',"");
 	document.getElementById('reformbut').style.display = "none";
 	document.getElementById('checkbut').style.display = "none";
 	document.getElementById('getCorrect').style.display = "none";
-	document.getElementById('answerArea').innerHTML = "";
-	document.getElementById('rightResult').innerHTML = "";
-	document.getElementById('wrongResult').innerHTML = "";
-	document.getElementById('correctSentence').innerHTML = "";
+	sentence = [];	
 }
-
+function reset(elem,to){
+	document.getElementById(elem).innerHTML = to;
+}
 
 function addWord(x){
 	var word = document.getElementById("btn"+x).value;
@@ -102,53 +153,35 @@ function addWord(x){
 }
 
 function checkCorrect(){
-	var array;
 	var formedSentence = sentence.join(" ");
 	formedSentence = formedSentence.trim();
 	if(lan==="English"){
 		array = eng[sent];
-		for(i=0;i<array.length;i++){
-			if(array[i]===formedSentence){
-				document.getElementById("rightResult").innerHTML = "Right Answer!!!";
-				document.getElementById("wrongResult").innerHTML = "";
-				return true;
-			}
-		}
-		document.getElementById("wrongResult").innerHTML = "Wrong Answer!!!";
-		document.getElementById("rightResult").innerHTML = "";
-		document.getElementById("getCorrect").style.display = "block";
-	}
-	else if(lan==="Hindi"){
+	}else if(lan==="Hindi"){
 		array = hin[sent];
+	}
 		for(i=0;i<array.length;i++){
 			if(array[i]===formedSentence){
-				document.getElementById("rightResult").innerHTML = "Right Answer!!!";
-				document.getElementById("wrongResult").innerHTML = "";
+				reset('rightResult',"Right Answer!!!");
+				reset('wrongResult',"");
 				return true;
 			}
-				
 		}
-		document.getElementById("wrongResult").innerHTML = "Wrong Answer!!!";
-		document.getElementById("rightResult").innerHTML = "";
+		reset('wrongResult',"Wrong Answer!!!");
+		reset('rightResult',"");
 		document.getElementById("getCorrect").style.display = "block";
 	}
-}
+
+
 
 function getCorrect(){
-	if(document.getElementById('getCorrect').innerHTML==="Get Correct Sentence"){
-	array = eng[sent];
+	if((document.getElementById('getCorrect').innerHTML==="Get Correct Sentence")||(document.getElementById('getCorrect').innerHTML === "Get Answers")){
 	for (i = 0; i < array.length; i++) {
 	document.getElementById('correctSentence').innerHTML += array[i]+"<br>";
 	}
-	document.getElementById('getCorrect').innerHTML = "Hide the Correct Sentence";
+	reset('getCorrect',"Hide the Correct Sentence");
 }else if(document.getElementById('getCorrect').innerHTML === "Hide the Correct Sentence"){
-	document.getElementById('correctSentence').innerHTML = "";
-	document.getElementById('getCorrect').innerHTML = "Get Answers";
-}else if(document.getElementById('getCorrect').innerHTML === "Get Answers"){
-	array = eng[sent];
-	for (i = 0; i < array.length; i++) {
-	document.getElementById('correctSentence').innerHTML += array[i]+"<br>";
-	}
-	document.getElementById('getCorrect').innerHTML = "Hide the Correct Sentence";
+	reset('correctSentence',"");
+	reset('getCorrect',"Get Answers");
 }
 }
