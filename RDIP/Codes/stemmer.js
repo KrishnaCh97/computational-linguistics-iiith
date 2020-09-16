dbbc3eabc8d76de153cb638cc21b5e038a1208ce
KrishnaCh97 <chskbharath@gmail.com>
2020-09-16 08:39:04 +0530

    Used snowball to find new types

--- RDIP/Codes/stemmer.js
+++ RDIP/Codes/stemmer.js
@@ -2,15 +2,17 @@
 var stemmer = new Snowball('english');
 
 
-var corp1 = 'A mouse was having a very bad time. She could find no food at all. She looked here and there, but there was no food, and she grew very thin. At last the mouse found a basket, full of corn. There was a small hole in the basket, and she crept in. She could just get through the hole. Then she began to eat the corn. Being very hungry, she ate a great deal, and went on eating and eating. She had grown very fat before she felt that she had had enough. When the mouse tried to climb out of the basket, she could not. She was too fat to pass through the hole. " How shall I climb out?" said the mouse. "oh, how shall I climb out?" Just then a rat came along, and he heard the mouse. "Mouse," said the rat, "if you want to climb out of the basket, you must wait till you have grown as thin as you were when you went in."';
+var corp1 = 'A mouse was having a very bad time. She could find no food at all. She looked here and there, but there was no food, and she grew very thin. At last the mouse found a basket, full of corn. There was a small hole in the basket, and she crept in. She could just get through the hole. Then she began to eat the corn. Being very hungry, she ate a great deal, and went on eating and eating. She had grown very fat before she felt that she had had enough. When the mouse tried to climb out of the basket, she could not. She was too fat to pass through the hole. "How shall I climb out?" said the mouse. "oh, how shall I climb out?" Just then a rat came along, and he heard the mouse. "Mouse," said the rat, "if you want to climb out of the basket, you must wait till you have grown as thin as you were when you went in."';
 var corp2 = "A wolf carried off a lamb. The lamb said,'I know you are going to eat me, but before you eat me I would like to hear you play the flute. I have heard that you can play the flute better than anyone else, even the shepherd himself.' The wolf was so pleased at this that he took out his flute and began to play. When he had done, the lamb insisted him to play once more and the wolf played again. The shepherd and the dogs heard the sound, and they came running up and fell on the wolf and the lamb was able to get back to the flock.";
 var corp3 = "A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it. Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. 'Why does he not make a pet of me?' said the donkey. 'It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master's knee. It is not fair.' Then the donkey said to himself, 'If I do what the dog does, he may make a pet of me.' So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table. Then it tried to jump on to its master's knee. The master thought the donkey was mad, and he shouted, 'Help! Help!' Men came running in with sticks, and they beat the donkey till it ran out of the house, and they drove it back to the field.'I only did what the dog does,' said the donkey, 'and yet they make a pet of the dog, and they beat me with sticks. It is not fair.'";
+
+
 var corp = "";
 var tokens = 0;
 var types = 0;
 var selected;
 var temp;
-var sys;
+var sys=[];
 function show(){
 	corp = document.getElementById('corpus').value;
 	document.getElementById('selectedCorp').style.display = "block";
@@ -90,8 +92,8 @@ function tokenCount(){
 	return tokens;
 }
 function typeCount(){
-	sys = [temp[0].toLowerCase()];
-	for(x=1;x<temp.length;x++){
+	sys = [];
+	for(x=0;x<temp.length;x++){
 		temp[x] = temp[x].toLowerCase();
 		if(!sys.includes(temp[x])){
 			sys.push(temp[x]);
@@ -131,24 +133,36 @@ function firstCheck(){
 		hideElem('cont');
 	}
 }
+var sysNew;
+var newTypeInput;
 function proceed(){
-	var sysNew=[];
+	sysNew=[];
 	set('answer',"Now, consider all the tokens with the same 'root' word to be of the same type. Recalculate the number of types.");
 	hideElem("cont");
 	hideElem("sub");
 	showElem("newTypes");
 	showElem("sub2");
-	var newTypeInput = document.getElementById('newTypes').value;
+
 	for(i=0;i<sys.length;i++){
-		stemmer.setCurrent(sys[i]);
-		stemmer.stem();
-		var x = stemmer.getCurrent();
-		console.log(x);
-		if(!sysNew.includes(x)){
-			sysNew.push(x);
+		var x = sys[i];
+		if((x!="the")&&(x!="to")&&(x!="of")&&(x!="very")&&(x!="you")&&(x!="does")&&
+			(x!="off")&&(x!="me")&&(x!="up")&&(x!="can")&&(x!="than")&&(x!="did")){
+			stemmer.setCurrent(x);
+			stemmer.stem();
+			var z = stemmer.getCurrent();
+			if(!sysNew.includes(z)){
+				sysNew.push(z);
+			}
 		}
 	}
-	console.log(sysNew.length);
+	
+}
+function secondCheck(){
+	newTypeInput = parseInt(document.getElementById('newTypes').value);
+	if(sysNew.length===newTypeInput){
+		document.getElementById('newTypes').style.background = "green";
+	}else
+		document.getElementById('newTypes').style.background = "red";
 }
 
 function theory(){
