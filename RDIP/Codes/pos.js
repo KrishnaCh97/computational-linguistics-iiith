31b30e142ae48fd59d841fa8b27131b476a52840
KrishnaCh97 <chskbharath@gmail.com>
2020-09-18 10:57:34 +0530

    Display Message and corresponding table

--- RDIP/Codes/pos.js
+++ RDIP/Codes/pos.js
@@ -21,14 +21,69 @@ function experiment(){
 }
 function showLan(){
 	var lan = document.getElementById("lan").value;
+	document.getElementById('instr').innerHTML="";
+	document.getElementById('lexTable').innerHTML ="";
+	document.getElementById('sub').style.display = "none";
 	if(lan==="select language"){
 		alert("Select Language");
 		return false;
 	}
-	else if(lan==="english"){
-		document.getElementById('selectedLan').innerHTML = "<select><option>---Select a sentence---</option><option>"+Eng[0]+"</option><option>"+Eng[1]+"</option><option>"+Eng[2]+"</option><option>"+Eng[3]+"</option><option>"+Eng[4]+"</option></select>"
-	}
 	else if(lan==="hindi"){
-		document.getElementById('selectedLan').innerHTML = "<select><option>---Select a sentence---</option><option>"+Hin[0]+"</option><option>"+Hin[1]+"</option><option>"+Hin[2]+"</option><option>"+Hin[3]+"</option><option>"+Hin[4]+"</option></select>"
-	}	
+		document.getElementById('selectedLan').innerHTML = "<select id='sent' name='sent' onclick='showTable()'><option value='select sentence'>---Select a sentence---</option><option value='hin0'>"+Hin[0]+"</option><option value='hin1'>"+Hin[1]+"</option><option value='hin2'>"+Hin[2]+"</option><option value='hin3'>"+Hin[3]+"</option><option value='hin4'>"+Hin[4]+"</option></select>"
+	}
+	else if(lan==="english"){
+		document.getElementById('selectedLan').innerHTML = "<select id='sent' name='sent' onclick='showTable()'><option value='select sentence'>---Select a sentence---</option><option value='eng0'>"+Eng[0]+"</option><option value='eng1'>"+Eng[1]+"</option><option value='eng2'>"+Eng[2]+"</option><option value='eng3'>"+Eng[3]+"</option><option value='eng4'>"+Eng[4]+"</option></select>"
+	}
+		
+}
+var sent ="";
+var selectedSent = [];
+function showTable(){
+
+	sent = document.getElementById('sent').value;
+	if(sent==="select sentence"){
+		alert("Select sentence");
+		return false;
+	}
+	else{
+		var row="";
+		document.getElementById('instr').innerHTML="Select the POS tag for corresponding words";
+		if(sent==="eng0"){
+			selectedSent = Eng[0].split(" ");
+		}
+		if(sent==="eng1"){
+			selectedSent = Eng[1].split(" ");
+		}
+		if(sent==="eng2"){
+			selectedSent = Eng[2].split(" ");
+		}
+		if(sent==="eng3"){
+			selectedSent = Eng[3].split(" ");
+		}
+		if(sent==="eng4"){
+			selectedSent = Eng[4].split(" ");
+		}
+		if(sent==="hin0"){
+			selectedSent = Hin[0].split(" ");
+		}
+		if(sent==="hin1"){
+			selectedSent = Hin[1].split(" ");
+		}
+		if(sent==="hin2"){
+			selectedSent = Hin[2].split(" ");
+		}
+		if(sent==="hin3"){
+			selectedSent = Hin[3].split(" ");
+		}
+		if(sent==="hin4"){
+			selectedSent = Hin[4].split(" ");
+		}
+		for(i=0;i<selectedSent.length;i++){row+="<tr><td>"+selectedSent[i]+"</td><td><select><option>Noun</option><option>Pronoun</option><option>Verb</option><option>Adverb</option><option>Adjective</option><option>Preposition</option><option>Postposition</option><option>Conjunction</option><option>Determiner</option><option>Interjection</option></select></td><td></td><td></td></tr>"};
+			document.getElementById('lexTable').innerHTML ="";
+			document.getElementById('lexTable').innerHTML = "<table><tr><th>LEXICON</th><th>POS</th><th></th><th></th></tr>"+
+		row
+		+"</table>"
+		document.getElementById('sub').style.display = "block";
+	}
+
 }
